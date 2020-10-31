b619ac02b2fa5cb1c3e173d3acb852fcce92eced
JuFeng Zhang <zjffun@gmail.com>
2020-10-31 11:03:30 +0800

    feat: add create date and updated date

--- themes/hexo-zjffun-note/scripts/init.js
+++ themes/hexo-zjffun-note/scripts/init.js
@@ -1,14 +1,25 @@
 'use strict';
 
 hexo.locals.set('zCategories', function () {
-  return hexo.locals.get('posts').reduce(function (res, post) {
-    var categoryTitle = post.path.split('/');
-    if (categoryTitle[2] !== undefined) {
-      if (!res[categoryTitle[0]]) {
-        res[categoryTitle[0]] = [];
+  var categroies = hexo.locals.get('posts').reduce(function (res, post) {
+    var categoryTitleInfo = post.path.split('/');
+    var categoryTitle = categoryTitleInfo[0];
+    if (categoryTitleInfo[2] !== undefined) {
+      if (!res[categoryTitle]) {
+        res[categoryTitle] = [];
       }
-      res[categoryTitle[0]].push(post);
+      res[categoryTitle].push(post);
     }
     return res;
   }, {});
+
+  Object.keys(categroies).forEach(function (key) {
+    var element = categroies[key];
+
+    categroies[key] = element.sort(function (a, b) {
+      return a.updated.isBefore(b.updated) ? 1 : -1;
+    });
+  });
+
+  return categroies;
 });
